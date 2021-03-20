36a174244edcb553743c701f9200633359813574
don_kanaille <60796711+don-kanaille@users.noreply.github.com>
2021-03-20 14:57:18 +0100

    New structure of app

--- app.py
+++ app.py
@@ -8,62 +8,15 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from flask import jsonify
-from flask_restful import Api
-from flask_jwt import JWT
-
-from src.security import authenticate, identity
-from src.resources.user import UserRegister
-from src.resources.item import Item, ItemList
-from src.resources.store import Store, StoreList
 from create_app import create_app
-from src.db import db
 
 
 __version__ = "0.1.0"
 __author__ = "github.com/don-kanaille"
 
 
-# Create app
+# Create & Initialize Flask app
 app = create_app(mode='TEST')
-
-
-# Initialization of .db, JWT & API
-db.init_app(app)
-jwt = JWT(app, authenticate, identity)
-api = Api(app=app)
-
-
-@app.before_first_request
-def create_tables() -> None:
-    """
-    Creates all the tables (it sees) in a .db file.
-    """
-    db.create_all()
-
-
-@jwt.auth_response_handler
-def customized_response_handler(access_token, identity_):
-    return jsonify({
-        'access_token': access_token.decode('utf-8'),
-        'user_id': identity_.id
-    })
-
-
-@jwt.jwt_error_handler
-def customized_error_handler(error):
-    return jsonify({
-        'message': error.description,
-        'code': error.status_code
-    }), error.status_code
-
-
-# Endpoints
-api.add_resource(UserRegister, '/register')
-api.add_resource(Item, '/item/<string:name>')
-api.add_resource(ItemList, '/items')
-api.add_resource(Store, '/store/<string:name>')
-api.add_resource(StoreList, '/stores')
 
 
 if __name__ == '__main__':
--- src/create_app.py
+++ src/create_app.py
@@ -1,5 +1,12 @@
-from flask import Flask
-from db import db
+from flask import jsonify, Flask
+from flask_restful import Api
+from flask_jwt import JWT
+
+from src.security import authenticate, identity
+from src.resources.user import UserRegister
+from src.resources.item import Item, ItemList
+from src.resources.store import Store, StoreList
+from src.db import db
 
 modes = {'PRODUCTION': 'ProductionConfig',
          'DEVELOP': 'DevelopmentConfig',
@@ -8,8 +15,8 @@ modes = {'PRODUCTION': 'ProductionConfig',
 
 def create_app(mode: str = 'DEPLOY') -> Flask:
     """
-    Creates a Flask app with specific configuration.
-    Default: DEPLOY.
+    Creates a Flask app with a specific configuration.
+    Default: PRODUCTION.
 
     :param mode: 'PRODUCTION', 'DEVELOP', 'TEST'
     :return: Flask app.
@@ -23,5 +30,38 @@ def create_app(mode: str = 'DEPLOY') -> Flask:
     # Load config
     app.config.from_object("config." + modes[mode])
     app.app_context().push()
+
+    # Initialization of .db, JWT & API
+    db.init_app(app)
+    jwt = JWT(app, authenticate, identity)
+    api = Api(app=app)
+
+    @app.before_first_request
+    def create_tables() -> None:
+        """
+        Creates all the tables (it sees) in a .db file.
+        """
+        db.create_all()
+
+    @jwt.auth_response_handler
+    def customized_response_handler(access_token, identity_):
+        return jsonify({
+            'access_token': access_token.decode('utf-8'),
+            'user_id': identity_.id
+        })
+
+    @jwt.jwt_error_handler
+    def customized_error_handler(error):
+        return jsonify({
+            'message': error.description,
+            'code': error.status_code
+        }), error.status_code
+
+    # Add Endpoints
+    api.add_resource(UserRegister, '/register')
+    api.add_resource(Item, '/item/<string:name>')
+    api.add_resource(ItemList, '/items')
+    api.add_resource(Store, '/store/<string:name>')
+    api.add_resource(StoreList, '/stores')
 
     return app
