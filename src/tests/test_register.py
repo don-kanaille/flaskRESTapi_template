06352d17b68aef4eb8436660038a2d23d41c830d
don_kanaille <60796711+don-kanaille@users.noreply.github.com>
2021-03-20 17:15:24 +0100

    Test for UserRegister

--- src/tests/test_register.py
+++ src/tests/test_register.py
@@ -1,11 +1,12 @@
-import unittest
 import json
 
 from BaseCase import BaseCase
 
 
 class TestUserRegister(BaseCase):
-
+    """
+    TestClass to test the register function of the app.
+    """
     def test_successful_register(self):
         # Given
         payload = json.dumps({
@@ -42,5 +43,33 @@ class TestUserRegister(BaseCase):
         response = self.app.post('/register', headers={"Content-Type": "application/json"}, data=payload)
 
         # Then
-        self.assertEqual({"message": "User created successfully."}, response.json)
-        self.assertEqual(201, response.status_code)
+        self.assertEqual({"username": "This field cannot be blank!"}, response.json['message'])
+        self.assertEqual(400, response.status_code)
+
+    def test_signup_without_password(self):
+        # Given
+        payload = json.dumps({
+            "username": "deep_thought"
+        })
+        # When
+        response = self.app.post('/register', headers={"Content-Type": "application/json"}, data=payload)
+
+        # Then
+        self.assertEqual({"password": "This field cannot be blank!"}, response.json['message'])
+        self.assertEqual(400, response.status_code)
+
+    def test_creating_already_existing_user(self):
+        # Given
+        payload = json.dumps({
+            "username": "deep_thought",
+            "password": "6942",
+        })
+        # Register user
+        response = self.app.post('/register', headers={"Content-Type": "application/json"}, data=payload)
+
+        # When
+        response = self.app.post('/register', headers={"Content-Type": "application/json"}, data=payload)
+
+        # Then
+        self.assertEqual({"message": "A user '{}' already exists!".format(json.loads(payload)['username'])}, response.json)
+        self.assertEqual(400, response.status_code)
