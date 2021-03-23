792afe99dee8a9c4876924a103d40592e640c16c
don_kanaille <60796711+don-kanaille@users.noreply.github.com>
2021-03-23 17:38:26 +0100

    Add test_succesful_user_deletion

--- src/test/test_delete_user.py
+++ src/test/test_delete_user.py
@@ -4,7 +4,25 @@ from test.test_basic import BaseCase
 
 
 class TestUserDelete(BaseCase):
-    def test_something(self):
-        ...
-        # TODO: write test to delete user from database
+    """
+    TestClass to test the user deletion function.
+    """
+    def test_successful_user_deletion(self):
+        # Given
+        username = 'userjw'
+        password = '1q2w3e4r'
+        payload = json.dumps({
+            'username': username,
+            'password': password
+        })
+        # Register
+        response = self.app.post('/register', headers={"Content-Type": "application/json"}, data=payload)
+        # Login
+        response = self.app.post('/login', headers={"Content-Type": "application/json"}, data=payload)
+        # When
+        user_id = response.json['user_id']
+        response = self.app.delete('/user/{}'.format(user_id), headers={}, data={})
 
+        # Then
+        self.assertEqual('User deleted.', response.json['message'])
+        self.assertEqual(200, response.status_code)
