cb99337dea80a7bcc8f8c20600aeb55f22268045
don_kanaille <60796711+don-kanaille@users.noreply.github.com>
2021-03-27 19:18:48 +0100

    Added @jwt decorators to customize errors

--- create_app.py
+++ create_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 
@@ -38,18 +38,53 @@ def create_app(mode: str = 'DEPLOY') -> Flask:
     jwt = JWTManager(app=app)
     api = Api(app=app)
 
-    @jwt.additional_claims_loader  # old: user_claims_loader
+    @jwt.additional_claims_loader
     def add_claims_to_jwt(identity: int) -> dict:
         """
         Whenever we create a new JWT-token, this function is called to check,
         if we should add any extra data ("claims") to that JWT as well.
 
         :param identity: Int of the user-id.
-        :return:
+        :return: {'is_admin': Bool}
         """
         if identity == 1:
             return {'is_admin': True}  # TODO: instead of hard-coding, read from .config
         return {'is_admin': False}
+
+    @jwt.expired_token_loader
+    def expired_token_callback() -> tuple:
+        return jsonify({
+            'description': 'The token has expired.',
+            'error': 'token_expired'
+        }), 401
+
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return jsonify({
+            'description': 'Signature verification failed.',
+            'error': 'token_invalid'
+        }), 401
+
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return jsonify({
+            'description': 'Request does not contain any access token.',
+            'error': 'authorization_required'
+        }), 401
+
+    @jwt.needs_fresh_token_loader
+    def token_not_fresh_callback(jwt_headers, jwt_payload):
+        return jsonify({
+            'description': 'Token is NOT fresh.',
+            'error': 'fresh_token_required'
+        }), 401
+
+    @jwt.revoked_token_loader
+    def revoked_token_callback(jwt_headers, jwt_payload):
+        return jsonify({
+            'description': 'Token has been revoked.',
+            'error': 'token_revoked'
+        }), 401
 
     @app.before_first_request
     def create_tables() -> None:
@@ -59,12 +94,12 @@ def create_app(mode: str = 'DEPLOY') -> Flask:
         db.create_all()
 
     @app.errorhandler(404)
-    def page_not_found(e) -> tuple:
+    def page_not_found(error) -> tuple:
         # TODO: log error + comments
         return render_template('error-404.html'), 404
 
     @app.errorhandler(500)
-    def internal_server_error(e) -> tuple:
+    def internal_server_error(error) -> tuple:
         # TODO: log error + comments
         return render_template('error-500.html'), 500
 
