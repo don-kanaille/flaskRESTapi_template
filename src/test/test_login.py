6900f3a64f2d01d53faf6ba605eb29677408fe0b
don_kanaille <60796711+don-kanaille@users.noreply.github.com>
2021-03-29 22:56:52 +0200

    test_jwt_token_were_created

--- src/test/test_login.py
+++ src/test/test_login.py
@@ -24,9 +24,8 @@ class TestUserLogin(BaseCase):
 
     def test_login_with_invalid_username(self):
         # Given
-        # TODO: adapt test
         payload = {
-            "email": "userjw",
+            "username": "userjw",
             "password": "1q2w3e4r"
         }
         response = self.app.post('/register', headers={"Content-Type": "application/json"}, data=json.dumps(payload))
@@ -36,14 +35,13 @@ class TestUserLogin(BaseCase):
         response = self.app.post('/login', headers={"Content-Type": "application/json"}, data=json.dumps(payload))
 
         # Then
-        self.assertEqual("Invalid credentials", response.json['message'])
+        self.assertEqual("Invalid credentials!", response.json['message'])
         self.assertEqual(401, response.status_code)
 
     def test_login_with_invalid_password(self):
         # Given
-        # TODO: adapt test
         payload = {
-            "email": "userjw",
+            "username": "userjw",
             "password": "1q2w3e4r"
         }
         response = self.app.post('/register', headers={"Content-Type": "application/json"}, data=json.dumps(payload))
@@ -53,5 +51,21 @@ class TestUserLogin(BaseCase):
         response = self.app.post('/login', headers={"Content-Type": "application/json"}, data=json.dumps(payload))
 
         # Then
-        self.assertEqual("Invalid credentials", response.json['message'])
+        self.assertEqual("Invalid credentials!", response.json['message'])
         self.assertEqual(401, response.status_code)
+
+    def test_jwt_token_were_created(self):
+        # Given
+        payload = {
+            "username": "userjw",
+            "password": "1q2w3e4r"
+        }
+        response = self.app.post('/register', headers={"Content-Type": "application/json"}, data=json.dumps(payload))
+
+        # When
+        response = self.app.post('/login', headers={"Content-Type": "application/json"}, data=json.dumps(payload))
+
+        # Then
+        self.assertTrue(response.json['access_token'])
+        self.assertTrue(response.json['refresh_token'])
+        self.assertEqual(200, response.status_code)
