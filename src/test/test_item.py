6a9034bf200fb32999cc6623255a3e75b27feba2
don_kanaille <60796711+don-kanaille@users.noreply.github.com>
2021-03-30 16:31:16 +0200

    test_successfully_create_item

--- src/test/test_item.py
+++ src/test/test_item.py
@@ -1,8 +1,48 @@
-import unittest
+import json
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
+from test.test_basic import BaseCase
+
+
+class TestItemCases(BaseCase):
+    """
+    TestClass to test CRUD item methods.
+    """
+    # Given
+    payload_login = json.dumps({
+        'username': 'userjw',
+        'password': '1q2w3e4r'
+    })
+
+    def test_successfully_create_item(self, payload_login=payload_login):
+        # Given
+        item = 'foobar'
+        payload = json.dumps({
+            "price": 42.69,
+            "store_id": 42
+        })
+        # Preconditions
+        response = self.app.post('/register', headers={"Content-Type": "application/json"}, data=payload_login)
+        response = self.app.post('/login', headers={"Content-Type": "application/json"}, data=payload_login)
+
+        # When
+        access_token = 'Bearer ' + response.json['access_token']
+        header = {"Authorization": access_token, "Content-Type": "application/json"}
+        response = self.app.post('/item/{}'.format(item), headers=header, data=payload)
+
+        # Then
+        self.assertEqual(1, response.json['id'])
+        self.assertEqual(42.69, response.json['price'])
+        self.assertEqual(42, response.json['store_id'])
+        self.assertEqual(201, response.status_code)
+
+    def test_duplicate_item(self):
+        # TODO
         self.assertEqual(True, False)
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_update_item(self):
+        # TODO
+        self.assertEqual(True, False)
+
+    def test_delete_item(self):
+        # TODO
+        self.assertEqual(True, False)
